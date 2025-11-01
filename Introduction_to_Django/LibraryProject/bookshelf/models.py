6378112210f119cb9ff4fe9fd91f0d7e9a9552c3
Cynthia Oyugi <cynthiaoyugi370@gmail.com>
2025-11-01 15:32:19 +0300

    Fixed list_filter and search_fields configuration for book model in admin

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
-    isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13, default='0000000000000')  # default added to avoid migration errors
 
     def __str__(self):
         return self.title
